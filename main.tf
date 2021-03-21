f49d3fadfc0384c18c1fd9e2db5a7fc5a979ef05
tiobagio <tio@hashicorp.com>
2021-03-21 10:36:59 +0800

    first commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "app" {
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-2a"
+  ami               = "ami-0c55b159cbfafe1f0"
+}
+
+
+#  tags = {
+#     Name = "app-${count.index}"
+#  }
+#}
